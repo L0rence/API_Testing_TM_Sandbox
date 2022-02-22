74815f7309fc93d6b8a9ce11022eb7a4355a0a22
L0rence <lawrence.chandru@gmail.com>
2022-02-23 09:54:23 +1300

    First Commit

---
+++ TM_SandBox_API_Testing/API/API_Demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using RestSharp;
+using TM_SandBox_API_Testing.DTO;
+using TM_SandBox_API_Testing.Helper;
+
+namespace TM_SandBox_API_Testing
+{
+    public class Demo<T>
+    {
+        public ListOfUsersDTO GetUser(string endpoint)
+        {
+            var user = new APIHelper<ListOfUsersDTO>();
+            var url = user.SetUrl(endpoint);
+            var request = user.CreateGetRequest();
+            var response = user.GetResponse(url, request);
+            ListOfUsersDTO content = user.GetContent<ListOfUsersDTO>((RestResponse)response);
+            return content;
+        }
+    }
+}
---
+++ TM_SandBox_API_Testing/Test/AcceptanceTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using TM_SandBox_API_Testing.DTO;
+
+namespace TM_SandBox_API_Testing
+{
+    public class Tests
+    {
+        [Test, Order(1)]
+        public void VerifyName()
+        {
+            var demo = new Demo<ListOfUsersDTO>();
+            var user = demo.GetUser("v1/Categories/6327/Details.json?catalogue=false");
+            System.Console.WriteLine("*******************             ******************* ");
+            System.Console.WriteLine("Name is : " + user.Name);
+            System.Console.WriteLine("*******************             ******************* ");
+            Assert.AreEqual("Basic", user.Promotions[0].Name);
+            Assert.AreEqual("Carbon credits", user.Name);
+
+
+        }
+
+        [Test, Order(2)]
+        public void VerifyCanRelist()
+        {
+            var demo = new Demo<ListOfUsersDTO>();
+            var user = demo.GetUser("v1/Categories/6327/Details.json?catalogue=false");
+            System.Console.WriteLine("*******************             ******************* ");
+            System.Console.WriteLine("CanRelist : " + user.CanRelist);
+            System.Console.WriteLine("*******************             ******************* ");
+            Assert.IsTrue(user.CanRelist);
+        }
+
+        [Test, Order(3)]
+        public void VerifyGallery()
+        {
+            var demo = new Demo<ListOfUsersDTO>();
+            var user = demo.GetUser("v1/Categories/6327/Details.json?catalogue=false");
+            // Printing the output at Console 
+            System.Console.WriteLine("*******************             ******************* ");
+            System.Console.WriteLine("Gallery and Description " + user.Promotions[1].Name  + " ==>> : "+ user.Promotions[1].Description);
+            System.Console.WriteLine("*******************             ******************* ");
+
+            Assert.AreEqual("Gallery", user.Promotions[1].Name);
+            Assert.AreEqual("Good position in category", user.Promotions[1].Description);
+
+        }
+
+
+    }
+}
